7de8b72382f5c4f6cb298217c40a838917215c02
sineoyku <sineoyku@gmail.com>
2025-07-18 20:35:26 +0300

    profile page

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,10 @@
 // Placeholder JS logic
 console.log("Bookshelf JS loaded");
+
+// Auto-hide flash messages after 4 seconds
+ document.querySelectorAll('.flash-messages li').forEach(msg => {
+   setTimeout(() => {
+     msg.style.opacity = '0';
+     setTimeout(()=> msg.remove(), 700); // remove after fade transition
+   }, 1000);
+ });
